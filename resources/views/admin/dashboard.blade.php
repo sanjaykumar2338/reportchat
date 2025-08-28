9075b3385ee6f788c46e02640c475f7733f4edc6
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2025-08-28 22:36:17 +0530

    change logo

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SafeTower Naucalpan - Admin Panel</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('images/fav.webp')}}">
+    <link rel="icon" type="image/x-icon" href="{{asset('images/logo.jpeg')}}">
 
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
